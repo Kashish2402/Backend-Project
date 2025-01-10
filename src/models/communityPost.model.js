d6394391cfa4d27eac530b5657fc593eb36ee3aa
Kashish2402 <Kashishgupta2402@gmail.com>
2025-01-10 19:11:58 +0530

    added comment, playlist, like, communityPost models

---
+++ src/models/communityPost.model.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+
+const communityPostSchema=new Schema(
+    {
+        owner:{
+            type:Schema.Types.ObjectId,
+            ref:"User",
+            required:true
+        },
+
+        content:{
+            type:String,
+            required:true
+        }
+    },
+    {
+        timestamps:true
+    }
+)
+
+export const communityPost=mongoose.model("CommunityPost",tweetSchema)
