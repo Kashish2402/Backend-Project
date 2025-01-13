d309ac7b464b7839b8cd9dc674053ca8686957da
Kashish2402 <Kashishgupta2402@gmail.com>
2025-01-13 19:53:36 +0530

    added like route

--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,6 +1,15 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/authentication.middleware.js";
+import { getLikedVideos, toggleCommentLike, toggleCommunityPostLike, toggleVideoLike } from "../controllers/like.controller.js";
 
 const router=Router()
 
 router.use((verifyJWT))
+
+router.route("/toggle/v/:videoId").post(toggleVideoLike)
+router.route("/toggle/c/:commentId").post(toggleCommentLike)
+router.route("/toggle/t/:postId").post(toggleCommunityPostLike)
+
+router.route("/videos").get(getLikedVideos)
+
+export default router
